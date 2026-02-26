6047afeb837c2a77b7b12b106e0060f86a52fa0e
matthewma1522-debug <matthewma1522@gmail.com>
2026-02-26 08:42:50 -0800

    a

--- my_bot.py
+++ my_bot.py
@@ -10,10 +10,18 @@ This function will be called every time anyone says anything on a channel where 
 * You can have the bot respond differently to different users
 """
 def should_i_respond(user_message, user_name):
-    if "robot" in user_message:
+    if "please replace all the vowels in this sentence with the letter u" in user_message:
       return True
-    else:
-        return False
+    if "please replace all the vowels in this sentence with the letter a" in user_message:
+        return True
+    if "1234567890" in user_message:
+        return True
+
+def recite_pi():
+   print(3.1415)
+def roll_dice():
+   return str(random.radint(1,6))
+
 """
 **Do NOT change the name of this function.**
 
@@ -23,10 +31,19 @@ This function will be called every time the `should_i_respond` function returns 
 * The bot will post the returned string on the channel where the original message was sent.
 * You can have the bot respond differently to different messages and users
 """
+import random
+def shuffle_numbers(text):
+    numbers = "1234567890"
+    random.shuffle(numbers)
+    shuffled = "".join(numbers)
+    return shuffled
+   
+
+
 
 
 def replace_vowels(text):
-    vowels = "aeiouAEIOU"
+    vowels = "aieouAIEOU"
     new_text = ""
 
     for char in text:
@@ -42,4 +59,13 @@ def replace_vowels(text):
 
 
 def respond(user_message, user_name):
-    return replace_vowels(user_message)
+    if "please replace all the vowels in this sentence with the letter u" in user_message:
+      return replace_vowels(user_message)
+    if "please replace all the vowels in this sentence with the letter a" in user_message:
+      return replace_vowels(user_message)
+    if "1234567890" in user_message:
+      return shuffle_numbers(user_message)
+    if "dice" in user_message:
+       return roll_dice
+    if "recite pi" in user_message:
+       return recite_pi
