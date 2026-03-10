775ce7c59ba32f5c85cab70ef2442cf90d9bf433
matthewma1522-debug <matthewma1522@gmail.com>
2026-03-10 09:30:55 -0700

    h

--- my_bot.py
+++ my_bot.py
@@ -3,6 +3,41 @@ from openai import OpenAI
 import os
 import random
 
+jokes = [
+    "why did the chicken cross the road? It wanted to go to the other side",
+    "why was the math book sad? Because it had too many problems."
+]
+def tell_joke():
+    return random.choice(jokes)
+def random_number():
+    return str(random.randint(1,100))
+def roll_dice():
+    return str(random.randint(1,6))
+def count_vowels(text):
+    vowels = "aeiou"
+    count = 0
+
+    for char in text.lower():
+        if char in vowels:
+            count += 1
+
+    return count
+def replace_vowels(text):
+    vowels = "aieouAIEOU"
+    new_text = ""
+
+    for char in text:
+        if char in vowels:
+            if char.isupper():
+                new_text += "U"
+            else:
+                new_text += "u"
+        else:
+            new_text += char
+
+    return new_text
+
+
 my_openai_key = secret.my_openai_key
 desired_words = 2000
 max_token_limit = int(desired_words * 1.4)
@@ -24,81 +59,22 @@ def get_ai_response(user_message, user_name):
     except Exception as e:
         print("OpenAI error:", repr(e))
         return "invalid"
-"""
-**Do NOT change the name of this function.**
-
-This function will be called every time anyone says anything on a channel where the bot lives.
-
-* It returns `True` if the bot notices something it wants to repond to.
-* You can have certain words or patterns in the messages trigger the bot.
-* You can have the bot respond differently to different users
-"""
-"""
-def should_i_respond(user_message, user_name):
-    if "please replace all the vowels in this sentence with the letter u" in user_message:
-      return True
-    if "please replace all the vowels in this sentence with the letter a" in user_message:
-        return True
-    if "1234567890" in user_message:
-        return True
-    if "bye" in user_message:
-        return True
-
-def recite_pi():
-   print(3.1415)
-def roll_dice():
-   return str(random.randint(1,6))
-
-
-**Do NOT change the name of this function.**
-
-This function will be called every time the `should_i_respond` function returns `True`.
-
-* This function returns a string.
-* The bot will post the returned string on the channel where the original message was sent.
-* You can have the bot respond differently to different messages and users
-
-def shuffle_numbers(text):
-    numbers = "1234567890"
-    random.shuffle(numbers)
-    shuffled = "".join(numbers)
-    return shuffled
-   
-
-
-
-
-def replace_vowels(text):
-    vowels = "aieouAIEOU"
-    new_text = ""
-
-    for char in text:
-        if char in vowels:
-            if char.isupper():
-                new_text += "U"
-            else:
-                new_text += "u"
-        else:
-            new_text += char
-
-    return new_text
-
+    
 
 def respond(user_message, user_name):
+    msg = user_message.lower()
+    if "hello" in msg:
+      return "Hello!"
     if "please replace all the vowels in this sentence with the letter u" in user_message:
       return replace_vowels(user_message)
-    if "please replace all the vowels in this sentence with the letter a" in user_message:
-      return replace_vowels(user_message)
-    if "1234567890" in user_message:
-      return shuffle_numbers(user_message)
+    if "number" in user_message:
+      return random_number()
     if "dice" in user_message:
        return roll_dice()
-    if "recite pi" in user_message:
-       return recite_pi()
     if "bye" in user_message:
-       print("bye")
-"""
-def respond(user_message, user_name):
+       return "bye"
+    if "how are you" in user_message:
+        return "I am your chatbot"
     return get_ai_response(user_message, user_name)
 def should_i_respond(user_message, user_name):
 
